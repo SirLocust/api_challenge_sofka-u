cfb27965d1590af106f0ea3a6c0e1a2315fadfb0
sirlocust <jua56yu@hotmail.com>
2021-07-07 18:10:23 -0500

    template basic api

---
+++ src/main/java/com/challenge/sofka/SofkaApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaApplication.class, args);
+	}
+
+}
